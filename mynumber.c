983574d217efe841e18c8bcd09cf0ef99ab7e7de
yuzeox <yooj102006@gmail.com>
2026-04-27 17:37:37 +0900

    4. add prime number print

--- mynumber.c
+++ mynumber.c
@@ -8,5 +8,16 @@ int main() {
 	for (int i = 1; i <= 100; i++) {
 		if (i % 2 == 0) printf("%d ", i);
 	}
+	printf("\n");
+	for (int i = 2; i <= 100; i++) {
+		int isPrime = 1;
+		for (int j = 2; j * j <= i; j++) {
+			if (i % j == 0) {
+				isPrime = 0;
+				break;
+			}
+		}
+		if (isPrime) printf("%d ", i);
+	}
 	return 0;
 }
