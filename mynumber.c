361e4234575e0f69bad754bf0d235b496853526a
yuzeox <yooj102006@gmail.com>
2026-04-27 17:27:00 +0900

    3. add even number print

--- mynumber.c
+++ mynumber.c
@@ -4,5 +4,9 @@ int main() {
 	for (int i = 1; i <= 100; i++) {
 		if (i % 2 != 0) printf("%d ", i);
 	}
+	printf("\n");
+	for (int i = 1; i <= 100; i++) {
+		if (i % 2 == 0) printf("%d ", i);
+	}
 	return 0;
 }
